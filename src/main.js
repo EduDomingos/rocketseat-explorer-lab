a3029a787af1611c44afda128b8ab77d7e97508a
Eduardo Domingos <eduardo.domingos.pb@compasso.com.br>
2022-11-04 19:03:50 -0300

    Segunda aula finalizada

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
 const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
@@ -18,3 +19,55 @@ function setCardType(type) {
 }
 
 globalThis.serCardType = setCardType
+
+const securityCode = document.querySelector('#security-code')
+const securityCodeMasked = IMask(securityCode, {mask: '000'})
+
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDateMasked = IMask(expirationDate, {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12
+    },
+
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    }
+  }
+})
+
+const cardNumber = document.querySelector('#card-number')
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: 'visa'
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: 'mastercard'
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: 'default'
+    }
+  ],
+
+  dispatch: (appended, dynamicMasked) => {
+    let number = (dynamicMasked.value + appended).replace(/\D/g, '')
+    let maskFound = dynamicMasked.compiledMasks.find(function(item) {
+      return number.match(item.regex)
+    })
+
+    return maskFound
+  }
+}
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
